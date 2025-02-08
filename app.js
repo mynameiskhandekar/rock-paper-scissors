82a6dbca40eb99635e318c266b578c360fa6eb35
Aditya Khandekar <khandekar.aditya1475@gmail.com>
2025-02-08 17:10:09 +0530

    created a function which returns computer response randomly in the rock paper scissors game.

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("Hello World")
+//Creating a function by which computer generates it's response.
+function getComputerChoice() {
+    let num = Math.random();
+    if(num < 1/3) {
+        return "rock";
+    } else if(num < 2/3) {
+        return "paper";
+    } else {
+        return "scissors"
+    }
+}
+console.log(getComputerChoice());
