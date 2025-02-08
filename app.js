da388d70af3e283f3d7d6d614e0cfc53e10c4225
Aditya Khandekar <khandekar.aditya1475@gmail.com>
2025-02-08 17:35:17 +0530

    Code for single round game

--- app.js
+++ app.js
@@ -27,3 +27,33 @@ function getHumanChoice() {
 //Scores
 let humanScore = 0;
 let computerScore = 0;
+
+//Creating logic to play single round
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+    if((humanChoice == 'rock') && (computerChoice == 'rock')) {
+        console.log("Tie!");
+    } else if((humanChoice == 'rock') && (computerChoice == 'paper')) {
+        console.log("You lose! Paper beats Rock");
+        computerScore++;
+    } else if((humanChoice == 'rock') && (computerChoice == 'scissors')) {
+        console.log("You win! Rock beats Scissors");
+        humanScore++;
+    } else if((humanChoice == 'paper') && (computerChoice == 'rock')) {
+        console.log("You win! Paper beats Rock");
+        humanScore++;
+    } else if((humanChoice == 'paper') && (computerChoice == 'paper')) {
+        console.log("Tie!");
+    } else if((humanChoice == 'paper') && (computerChoice == 'scissors')) {
+        console.log("You lose! Scissors beats Paper");
+        computerScore++;
+    } else if((humanChoice == 'scissors') && (computerChoice == 'rock')) {
+        console.log("You lose! Rock beats Scissors");
+    } else if((humanChoice == 'scissors') && (computerChoice == 'paper')) {
+        console.log("You win! Scissors beats Paper");
+    } else if((humanChoice == 'scissors') && (computerChoice == 'scissors')) {
+        console.log("Tie!");
+    } else {
+        console.log("Invalid");
+    }
+}
